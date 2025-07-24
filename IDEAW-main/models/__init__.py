2d9bb0734943f09b03c556765126ff78184c95c3
Umar Shaheen <umarshaheen@Umars-MacBook-Air.local>
2025-07-24 12:02:23 +0800

    deleted the IDEAW-main files from repository
    clean up the repo and get rid of unadjusted IDEAW files

--- IDEAW-main/models/__init__.py
+++
@@ -1,9 +0,0 @@
-from . import attackLayer, componentNet, dense, ideaw, innBlock, mihnet, utils
-
-
-# from .ideaw import IDEAW
-# from .dense import DenseBlock
-# from .inn import InnBlock
-# from .mihnet import Mihnet_s1, Mihnet_s2
-# from .componentNet import Discriminator, Restorer
-# from .attackLayer import AttackLayer
