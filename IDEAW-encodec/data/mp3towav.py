07859062517b0a4d85aa003d136bf4a4507c388c
UmarShaheen03 <umar.shaheen93@gmail.com>
2025-08-20 10:51:11 +0800

    "created mp3towav to covert mp3 audio into wav and resampled to 16000. Adjusted config to suit"

--- IDEAW-encodec/data/mp3towav.py
+++ IDEAW-encodec/data/mp3towav.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     with open(config_path) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
-        data_path = config["data_path"]
+        data_path = config["mp3_path"]
 
         audio_path_list = []  # save absolute paths of audio files
         for root_path, dirs, files in os.walk(data_path):
@@ -27,9 +27,10 @@ if __name__ == "__main__":
     for i, audio_path in enumerate(audio_path_list):
         try:
             song = AudioSegment.from_mp3(audio_path)
+            newsong = song.set_frame_rate(16000).set_channels(1)
             newpath = audio_path.split(".")[1]
             filename = export_path+"/"+ newpath.split("/")[-1]+".wav"
-            newwav = song.export(filename, format="wav")
+            newsong.export(filename, format="wav")
         except Exception as e:
             print(f"[WARN] Skipping {audio_path} ({e})")
             
